a91ab425e66372d34b82a93e6b933c5d8824101d
WinnieQueen <ashilliard@student.neumont.edu>
2020-10-29 16:54:26 -0600

    parsing out ORI codes now works

--- Safecity/SearchPage.xaml.cs
+++ Safecity/SearchPage.xaml.cs
@@ -11,6 +11,9 @@ using System.Net;
 using System.IO;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using AngleSharp.Html.Parser;
+using AngleSharp.Html.Dom;
+using AngleSharp.Dom;
 
 namespace Safecity
 {
@@ -31,75 +34,84 @@ namespace Safecity
             Application.Current.MainPage.Navigation.PushAsync(new HelpPage());
         }
 
-
-        /*{"zip_code":"08057",
-         * "lat":39.979676,
-         * "lng":-74.941163,
-         * "city":"Moorestown",
-         * "state":"NJ",
-         * "timezone":  {
-         *      "timezone_identifier":"America\/New_York",
-         *      "timezone_abbr":"EDT",
-         *      "utc_offset_sec":-14400,
-         *      "is_dst":"T"
-         *},
-         *"acceptable_city_names":[
-         *      {   "city":"Lenola",
-         *          "state":"NJ"
-         *      }],
-         *"area_codes":[856]}
-        */
-
-        private string GetORI(string state, string county, string city)
+        private List<string> GetORI(string state, string county, string city)
         {
+            List<string> ORIs = null;
+            string allStates = "https://www.icpsr.umich.edu/files/NACJD/ORIs/STATESoris.html";
+
             using (WebClient client = new WebClient())
             {
-                string htmlCode = client.DownloadString("https://www.icpsr.umich.edu/files/NACJD/ORIs/0  1oris.html");
-                if (htmlCode.Contains("ASHVILLE"))
-                    Console.Write(htmlCode);
+                string htmlString = client.DownloadString(allStates);
+                HtmlParser parser = new HtmlParser();
+                IHtmlDocument document = parser.ParseDocument(htmlString);
+                IElement[] links = document.Links.Where(x => x.InnerHtml.ToLower().Contains(state)).ToArray();
+                if (links != null && links.Length > 0)
+                {
+                    string linkPiece = links[0].GetAttribute("href");
+                    string stateUrl = $"https://www.icpsr.umich.edu/files/NACJD/ORIs/{linkPiece}";
+                    string stateHtml = client.DownloadString(stateUrl);
+                    document = parser.ParseDocument(stateHtml);
+                    links = document.Links.Where(link => link.InnerHtml.ToLower().Contains(county)).ToArray();
+                    if (links != null && links.Length > 0)
+                    {
+                        foreach (IElement link in links)
+                        {
+                            string name = link.GetAttribute("href");
+                            name = name.Substring(1);
+                            name = name.Replace("(", "\\(");
+                            name = name.Replace(")", "\\)");
+
+                            string pattern = $"(<h3><a name=\"{name}\">(.*\\s*)*?<pre>(.*\\s*)*?CITY\\/AGENCY(.*\\s*)*?\\s*ORI7\\s*ORI9\\s*)((.*\\s*)*?(?=<\\/pre>))";
+                            Regex r = new Regex(pattern);
+                            string infoString = r.Match(stateHtml).Groups[5].Value;
+
+                            string[] strings = Regex.Split(infoString, @"\s{2,}");
+                            if (strings != null && strings.Length >= 3)
+                            {
+                                ORIs = new List<string>();
+                                for (int i = 2; i < strings.Length; i += 2)
+                                {
+                                    Console.Write(strings[i] + ", ");
+                                    ORIs.Add(strings[i]);
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
             }
-            return "";
+            return ORIs;
         }
 
         private string[] GetInfo(int zip)
         {
-            try
-            {
-                //getting the state/city from APIs
-                string url = $"https://redline-redline-zipcode.p.rapidapi.com/rest/info.json/{zip}/degrees";
-                Console.WriteLine(url);
-                RestClient client = new RestClient(url);
-                RestRequest request = new RestRequest(Method.GET);
-                Console.WriteLine("so far so good");
-                request.AddHeader("x-rapidapi-host", "redline-redline-zipcode.p.rapidapi.com");
-                request.AddHeader("x-rapidapi-key", "ebacc40a32mshdcb364465409375p1f8018jsncc41302b2150");
-                request.AddHeader("useQueryString", "true");
+            //getting the city from APIs
+            string url = $"https://redline-redline-zipcode.p.rapidapi.com/rest/info.json/{zip}/degrees";
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "redline-redline-zipcode.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "ebacc40a32mshdcb364465409375p1f8018jsncc41302b2150");
+            request.AddHeader("useQueryString", "true");
 
-              
-                IRestResponse response = client.Execute(request);
-                var jObject = JObject.Parse(response.Content);
-                string city = jObject.GetValue("city").ToString();
-                city = city.ToLower();
-                string state = jObject.GetValue("state").ToString();
-                state = state.ToLower();
+            IRestResponse response = client.Execute(request);
+            var jObject = JObject.Parse(response.Content);
+            string city = jObject.GetValue("city").ToString();
+            city = city.ToLower();
 
-                //getting the county from database
-                string allInfo = Safecity.Properties.Resources.uszips;
-                string pattern = $"(\"{zip}\".*{state}.*{{)";
-                Regex r = new Regex(pattern);
-                string infoString = r.Match(allInfo.ToLower()).Value;
-                Console.WriteLine(infoString);
-                string[] cityInfo = infoString.Split(',');
-                string county = cityInfo[cityInfo.Length - 2];
-                county = county.Replace("\"", "");
+            //getting the state/county from database
+            string allInfo = Properties.Resources.uszips;
+            string pattern = $"(\"{zip}\".*{city}.*{{)";
+            Regex r = new Regex(pattern);
+            string infoString = r.Match(allInfo.ToLower()).Value;
+            string[] cityInfo = infoString.Split(',');
+            string county = cityInfo[cityInfo.Length - 2];
+            county = county.Replace("\"", "");
+            county = county.ToLower();
+            string state = cityInfo[cityInfo.Length - 8];
+            state = state.Replace("\"", "");
+            state = state.ToLower();
 
-                return new string[] { state, county, city };
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                return null;
-            }
+            return new string[] { state, county, city };
         }
 
         private Dictionary<string, string> GetData(int zip, int age, string sex, string race)
@@ -107,15 +119,22 @@ namespace Safecity
             Dictionary<string, string> data = null;
             if (zip >= 00501 && zip <= 99950)
             {
+                //hardcoded for testing
+                //string[] info = { "utah", "emery", "huntington" };
+
+
                 string[] info = GetInfo(zip);
                 if (info != null)
                 {
-                    data = new Dictionary<string, string>();
-                    string state = info[0];
-                    string county = info[1];
-                    string city = info[2];
-                    Console.WriteLine("State: " + state + "County: " + county + "City: " + city);
-                    return null;
+                    string state = info[0].ToLower();
+                    string county = info[1].ToLower();
+                    string city = info[2].ToLower();
+                    List<string> ORIs = GetORI(state, county, city);
+                    if (ORIs != null && ORIs.Count >= 1)
+                    {
+                        data = new Dictionary<string, string>();
+
+                    }
                 }
             }
             return data;
@@ -132,15 +151,17 @@ namespace Safecity
                 Dictionary<string, string> results = GetData(zip, age, sex, race);
                 if (results == null)
                 {
-                    ErrorMsg.IsVisible = true;
+                    //ErrorMsg.IsVisible = true;
                 }
                 else
                 {
                     Application.Current.MainPage.Navigation.PushAsync(new ResultsPage());
                 }
             }
-            catch (Exception)
+            catch (Exception error)
             {
+                Console.WriteLine(error.Message);
+                Console.WriteLine(error.ToString());
                 ErrorMsg.IsVisible = true;
             }
         }
