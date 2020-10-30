ea4f123a1ce2e4934a440a9129e40c1f983a96fc
WinnieQueen <ashilliard@student.neumont.edu>
2020-10-30 00:36:26 -0600

    WOOHOOgit add . LOADING BARgit add .git add .git add . AAAAAAAgit add .git add .

--- Safecity/SearchPage.xaml.cs
+++ Safecity/SearchPage.xaml.cs
@@ -14,6 +14,9 @@ using System.Text.RegularExpressions;
 using AngleSharp.Html.Parser;
 using AngleSharp.Html.Dom;
 using AngleSharp.Dom;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Safecity
 {
@@ -69,7 +72,7 @@ namespace Safecity
                             if (strings != null && strings.Length >= 3)
                             {
                                 ORIs = new List<string>();
-                                for (int i = 2; i < strings.Length; i += 2)
+                                for (int i = 2; i < strings.Length; i += 3)
                                 {
                                     Console.Write(strings[i] + ", ");
                                     ORIs.Add(strings[i]);
@@ -83,7 +86,7 @@ namespace Safecity
             return ORIs;
         }
 
-        private string[] GetInfo(int zip)
+        private string[] GetInfo(string zip)
         {
             //getting the city from APIs
             string url = $"https://redline-redline-zipcode.p.rapidapi.com/rest/info.json/{zip}/degrees";
@@ -114,48 +117,78 @@ namespace Safecity
             return new string[] { state, county, city };
         }
 
-        private Dictionary<string, string> GetData(int zip, int age, string sex, string race)
+        private Dictionary<string, string> GetData(string zip, int age, string sex, string race)
         {
             Dictionary<string, string> data = null;
-            if (zip >= 00501 && zip <= 99950)
+            //hardcoded for testing
+            //string[] info = { "utah", "emery", "huntington" };
+
+            string[] info = GetInfo(zip);
+            if (info != null)
             {
-                //hardcoded for testing
-                //string[] info = { "utah", "emery", "huntington" };
-
-
-                string[] info = GetInfo(zip);
-                if (info != null)
+                string state = info[0].ToLower();
+                string county = info[1].ToLower();
+                string city = info[2].ToLower();
+                List<string> ORIs = GetORI(state, county, city);
+                if (ORIs != null && ORIs.Count >= 1)
                 {
-                    string state = info[0].ToLower();
-                    string county = info[1].ToLower();
-                    string city = info[2].ToLower();
-                    List<string> ORIs = GetORI(state, county, city);
-                    if (ORIs != null && ORIs.Count >= 1)
-                    {
-                        data = new Dictionary<string, string>();
-
-                    }
+                    data = new Dictionary<string, string>();
                 }
             }
             return data;
         }
 
+        private void StartBar()
+        {
+            loadingBar.Progress = 0;
+            loadingBar.ProgressTo(1, 6000, Easing.Linear);
+        }
+
         private void Submit_Clicked(object sender, EventArgs e)
         {
+            Dictionary<string, string> results = null;
+            loadingBar.Progress = 0;
+            Button button = sender as Button;
+            button.IsEnabled = false;
             try
             {
                 int zip = Int32.Parse(Zip.Text);
-                int age = Age.Text == null ? 0 : Int32.Parse(Age.Text);
-                string sex = SexPicker.SelectedItem != null ? SexPicker.SelectedItem.ToString() : "";
-                string race = RacePicker.SelectedItem != null ? RacePicker.SelectedItem.ToString() : "";
-                Dictionary<string, string> results = GetData(zip, age, sex, race);
-                if (results == null)
+                if (zip >= 00501 && zip <= 99950)
                 {
-                    //ErrorMsg.IsVisible = true;
-                }
-                else
-                {
-                    Application.Current.MainPage.Navigation.PushAsync(new ResultsPage());
+                    int counter = 1;
+                    int age = Age.Text == null ? 0 : Int32.Parse(Age.Text);
+                    string sex = SexPicker.SelectedItem != null ? SexPicker.SelectedItem.ToString() : "";
+                    string race = RacePicker.SelectedItem != null ? RacePicker.SelectedItem.ToString() : "";
+                    Task<bool> barProgress = loadingBar.ProgressTo(1, 7000, Easing.Linear);
+                    ThreadPool.QueueUserWorkItem(o =>
+                    {
+                        results = GetData(Zip.Text, age, sex, race);
+                        counter = Interlocked.Decrement(ref counter);
+                    });
+
+                    barProgress.ContinueWith(o =>
+                    {
+                        while (counter != 0)
+                        {
+                            Console.WriteLine(counter);
+                            //do do do... waiting waiting waiting.... la la la
+                        }
+                        if (results == null)
+                        {
+                            //ErrorMsg.IsVisible = true;
+                        }
+                        else
+                        {
+                            barProgress.Wait();
+                            loadingBar.Progress = 0;
+                            button.IsEnabled = true;
+                            Device.BeginInvokeOnMainThread(() =>
+                            {
+                                Application.Current.MainPage.Navigation.PushAsync(new ResultsPage());
+                            });
+                        }
+                    });
+
                 }
             }
             catch (Exception error)
@@ -164,6 +197,19 @@ namespace Safecity
                 Console.WriteLine(error.ToString());
                 ErrorMsg.IsVisible = true;
             }
+            button.IsEnabled = true;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
